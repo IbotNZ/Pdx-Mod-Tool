d4339a8009598dd7c8e4c5ace2ba007554d9a77e
KiwiBot <tobi.hegglun@gmail.com>
2026-03-16 20:08:22 +1300

    Erased Text Reader Class
    Decided to approach the tokenize function without a class object for text reading.

--- lexer.cpp
+++ lexer.cpp
@@ -43,8 +43,6 @@ vector<token> tokenize(string txt) {
     // Denotes what type the lexer assumes the current char is from
     token_type state = WHITESPACE;
     vector<token> token_list;
-    char current_char;
-    int pos = 0, line = 1, col = 0;
 
     switch (state) {
         case WHITESPACE:
@@ -63,24 +61,3 @@ vector<token> tokenize(string txt) {
 
     return token_list;
 }
-
-// Probably will need to make multiple tokenizers due to how paradox script works.
-// So having a standalone object for string reading will be useful.
-class InputStream {
-    private:
-        int pos = 0, line = 1, col = 0;
-        char current_char, next_char;
-        string txt;
-
-        InputStream(string txt_source) {
-            txt = txt_source;
-            current_char = txt[0];
-            next_char = txt[1];
-        }
-
-        char next();
-
-        bool check_eof();
-
-        void croak();
-};
--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-    cout << stringify_file("ck3_history_char_example.txt", true);
+    int pos = 0;
+    char current_char, next_char;
+    string txt = stringify_file("ck3_history_char_example.txt");
+    current_char = txt[pos++];
+    next_char = txt[pos];
+    cout << current_char << next_char;
     return 0;
 }
